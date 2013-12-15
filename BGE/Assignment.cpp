376a782a271800d3f3ac79be844109c985d6efc7
Reggad <matallen91@hotmail.com>
2013-12-15 15:44:10 +0000

    added comments to assignment.cpp

--- BGE/Assignment.cpp
+++ BGE/Assignment.cpp
@@ -20,7 +20,14 @@
 #include "Utils.h"
 
 using namespace BGE;
-
+/*
+	This project was mostly a mish-mash of different things. It includes the code for the ships moving by applying force and the slerping code. 
+	There are also some particle affects.
+	The two main focuses of the project where a reuseable ragdoll (coded in physicsFactory.cpp) 
+	And
+	An attempt at a trebuchet game where a user would fire a medieval catapult at a wall. This only got as far as getting a basic trebuchet finished
+	as a huge amount of time was invested in trying to get the trebuchet to reset after it was fired.
+*/
 
 Assignment::Assignment(void)
 {
@@ -33,21 +40,26 @@ Assignment::Assignment(void)
 	elapsed = 10000;
 }
 
-	string tag;	
+	string tag; //string used in an attempt to reset the tags for certian rigid bodies so they could be deleted at will.
 
-	btHingeConstraint * plclasp;
+	//Different joints. Although one variable could be used to for all the joints by giving each joint it's own variable it makes it easier to
+	//delete the joints.
+	btHingeConstraint * plclasp; //payload joint
 	btHingeConstraint * hinge1;
 	btHingeConstraint * hinge2;
 	btHingeConstraint * hinge3;
 	btHingeConstraint * hinge4;
 	btHingeConstraint * hinge5;
 
+	//Trebuchet boxes
 	shared_ptr<PhysicsController> weight;
 	shared_ptr<PhysicsController> support;
 	shared_ptr<PhysicsController> arm;
 	shared_ptr<PhysicsController> sling;
 	shared_ptr<PhysicsController> payload;
 
+	//inital positions for the different parts of the trebuchet. These variables were set up in the hopes of being able to reset 
+	//the trebuchet after firing. However my attempts were unsuccessful.
 	glm::vec3 start_support;
 	glm::vec3 start_arm;
 	glm::vec3 start_weight;
@@ -59,10 +71,11 @@ Assignment::Assignment(void)
 Assignment::~Assignment(void)
 {
 }
-	bool pressed = false;
 
-//shared_ptr<PhysicsController> cyl;
-//std::shared_ptr<GameComponent> station;
+	
+	bool pressed = false;	//flag used to determine if a button has been pressed. This is used when detaching the payload
+							//so the program does not try to remove the hinge joint more than once as this could cause a crash.
+
 
 bool Assignment::Initialise() 
 {
@@ -71,7 +84,6 @@ bool Assignment::Initialise()
     collisionConfiguration = new btDefaultCollisionConfiguration();
     dispatcher = new btCollisionDispatcher(collisionConfiguration);
 	
-	tag = "tBox";
 
     // The world.
 	btVector3 worldMin(-1000,-1000,-1000);
@@ -87,22 +99,27 @@ bool Assignment::Initialise()
 	physicsFactory->CreateGroundPhysics();
 	physicsFactory->CreateCameraPhysics();
 
-	//shared_ptr<PhysicsController> origin = physicsFactory->CreateBox(1,1,1,0, glm::vec3(0, 0, 0), glm::quat()); 
-
-	//physicsFactory->CreateWall(glm::vec3(50,0,20), 10, 10);
-
+	//Initializing starting positions
 	start_support = glm::vec3(5, 10, 0);
 	start_arm = glm::vec3(1, 13, 2.5);
 	start_weight = glm::vec3(12, 13, 2.5);
 	start_sling = glm::vec3(-7, 6.5, 2.5);
 	start_payload = glm::vec3(-5, 1, 2.5);
 
-	support = physicsFactory->CreateTBox(1,10,1,0, start_support, glm::quat()); 
+	//Creating the structure of the trebuchet. Notice that they use the "CreateTBox" function. This is a modified 
+	//version of the CreateBox function in physicsFactory. The only two differences are that it takes an extra 
+	//parameter for the mass of the box and the tag has been changed to "TBox". This tag is used later when trying to delete
+	//the trebuchet.
+	support = physicsFactory->CreateTBox(1,10,1,0, start_support, glm::quat()); //support arm doesn't move so its mass is set to 0
 	arm = physicsFactory->CreateTBox(15,1,1,1, start_arm, glm::quat()); 
 	weight = physicsFactory->CreateTBox(5,3,1.5,1, start_weight, glm::quat()); 
 	sling = physicsFactory->CreateTBox(1,10,1,1, start_sling, glm::quat()); 
 	payload = physicsFactory->CreateTBox(1,1,1,1, start_payload, glm::quat()); 
-/*
+
+
+/*	Initial attempt at creating a "rope" for the trebuchet by connecting several boxes together. This was not used as it did not work as intended.
+	A standard box worked fine in the end.
+
 	shared_ptr<PhysicsController> seg1 = physicsFactory->CreateBox(1,1,1, glm::vec3(-6.5, 2, 2.5), glm::quat()); 
 	shared_ptr<PhysicsController> seg2 = physicsFactory->CreateBox(1,1,1, glm::vec3(-4.5, 2, 2.5), glm::quat()); 
 	shared_ptr<PhysicsController> seg3 = physicsFactory->CreateBox(1,1,1, glm::vec3(-2.5, 2, 2.5), glm::quat()); 
@@ -112,7 +129,7 @@ bool Assignment::Initialise()
 	shared_ptr<PhysicsController> seg7 = physicsFactory->CreateBox(1,1,1, glm::vec3(5.5, 2, 2.5), glm::quat()); 
 	shared_ptr<PhysicsController> seg8 = physicsFactory->CreateBox(1,1,1, glm::vec3(7.5, 2, 2.5), glm::quat()); 
 */
-
+	//Setting up all the hinge joints
 	hinge1 = new btHingeConstraint(*arm->rigidBody, *weight->rigidBody, btVector3(9,0,0.0f),btVector3(-3,0,0.0f), btVector3(0,1,0), btVector3(0,0,1), true);
 	dynamicsWorld->addConstraint(hinge1);
 	hinge2 = new btHingeConstraint(*arm->rigidBody, *sling->rigidBody, btVector3(-8,0,0),btVector3(0,6.5,0), btVector3(0,1,0), btVector3(0,0,1), true);
@@ -123,6 +140,9 @@ bool Assignment::Initialise()
 	dynamicsWorld->addConstraint(plclasp);
 
 /*
+
+	Hinge joints for the rope
+	
 	hinge = new btHingeConstraint(*box3->rigidBody, *seg1->rigidBody, btVector3(-7.5,-2,0.0f),btVector3(0,8,0.0f), btVector3(1,0,0), btVector3(1,0,0), true);
 	dynamicsWorld->addConstraint(hinge);
 	hinge = new btHingeConstraint(*seg1->rigidBody, *seg2->rigidBody, btVector3(1,0,0.0f),btVector3(-1,0,0.0f), btVector3(1,0,0), btVector3(1,0,0), true);
@@ -140,16 +160,16 @@ bool Assignment::Initialise()
 	hinge = new btHingeConstraint(*seg7->rigidBody, *seg8->rigidBody, btVector3(1,0,0.0f),btVector3(-1,0,0.0f), btVector3(1,0,0), btVector3(1,0,0), true);
 	dynamicsWorld->addConstraint(hinge);
 */
-	//physicsFactory->CreateRagDoll(glm::vec3(10,10,0));
-	//physicsFactory->CreateRagDoll(glm::vec3(20,10,0));
+
+	//Calls the ragdoll function I created in physicsFactory
 	physicsFactory->CreateRagDoll(glm::vec3(30,10,20));
 	
-	
+	//Makes pretty snow
 	shared_ptr<SnowEffect> snow = make_shared<SnowEffect>();
 	Attach(snow);
 
 
-
+	//Setting up ships as per the labs
 	ship1 = make_shared<GameComponent>();
 	ship1->Attach(Content::LoadModel("cobramk3", glm::rotate(glm::mat4(1), 180.0f, glm::vec3(0,1,0))));
 	ship1->position = glm::vec3(-10, 2, -10);
@@ -167,6 +187,7 @@ bool Assignment::Initialise()
 	slerping = false;
 	t = 0.0f;
 
+	//Creating a particle effect to represent smoke coming out of the back of the ship.
 	trail = make_shared<FountainEffect>(500);
 	trail->position = ship1->position;
 	Attach(trail);
@@ -200,6 +221,8 @@ void BGE::Assignment::Update(float timeDelta)
 
 	float newtons = 10.0f;
 	float epsilon = glm::epsilon<float>();
+
+	//Code to move ship
 	if (keyState[SDL_SCANCODE_UP])
 	{
 		force += ship1->look * newtons;
@@ -241,7 +264,7 @@ void BGE::Assignment::Update(float timeDelta)
 
 	
 
-	
+		//Code to detach payload
 		if (keyState[SDL_SCANCODE_K])
 		{
 			
@@ -252,23 +275,30 @@ void BGE::Assignment::Update(float timeDelta)
 			}
 		}
 
+		//Applies downward force to the counterweight to make the trebuchet swing. Sometimes it doesn't work for some unknown reason.
 		if (keyState[SDL_SCANCODE_L])
 		{
 			float f = 500.0f;
 			glm::vec3 down = glm::vec3(0,-1,0);
 			weight->rigidBody->applyCentralForce(GLToBtVector(down) * f);
-			//weight->rigidBody->applyTorque(GLToBtVector(glm::vec3(0.0f,1.0f,0.0f)));
 		}
 
+		//Code to reset the trebuchet.This is where the majority of the time spent on this project went. Originally I tried just reseting
+		//the positions of the boxes back to their starting positions but that didn't seem to do anything. The code below is an attempt at removing the
+		//trebuchet and then recreating it back at the starting positions. I never got as far as writing the code to rebuild the trebuchet.
 		if (keyState[SDL_SCANCODE_R])
 		{
 			list<shared_ptr<GameComponent>>::iterator it = children.begin();
-						dynamicsWorld->removeConstraint(hinge1);
+			
+			//Removing the joints so the program doesn't crash when the boxes are deleted.
+			dynamicsWorld->removeConstraint(hinge1);
 			dynamicsWorld->removeConstraint(hinge2);
 			dynamicsWorld->removeConstraint(hinge3);
 
 			 while (it != children.end())
 			 {
+					//iterate through the list of game components and if a gamecomponent has the tag "tBox" remove it.7
+					//For some reason this wouldn't work though.
 					 shared_ptr<GameComponent> component = * it;
 					 string t = component->tag;
 					 if ((component->tag == "tBox"))
@@ -288,12 +318,6 @@ void BGE::Assignment::Update(float timeDelta)
 
 
 
-			/*
-			dynamicsWorld->removeRigidBody(sling->rigidBody);
-			dynamicsWorld->removeRigidBody(weight->rigidBody);
-			dynamicsWorld->removeRigidBody(arm->rigidBody);
-			dynamicsWorld->removeRigidBody(support->rigidBody);
-			*/
 		}
 	// Now calculate the acceleration, new velocity and new position
 	glm::vec3 accel = force / mass;
